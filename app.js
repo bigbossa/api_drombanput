c539111bd028ed37f9be0d9f86b7b006d8952fe4
JTP.Codeing <110913626+bigbossa@users.noreply.github.com>
2025-07-27 22:37:07 +0700

    url

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ['https://domini-vercel.vercel.app','http://localhost:8080'],
+  origin: ['https://drom-banput.vercel.app','http://localhost:8080'],
   methods: ['GET', 'POST', 'PUT']
 }));
 
